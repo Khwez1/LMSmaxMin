7705031c2b6e61c9290ffdc04c6c1242be2601f0
Khwez1 <khwezimbobo@gmail.com>
2023-11-27 10:24:23 +0200

    made some changes

--- index.js
+++ index.js
@@ -1,20 +1,3 @@
-// let array = [0,10,4,7,24,38,14,43,56];
-// let Btn1 = document.getElementById('btn1');
-// let Btn2 = document.getElementById('btn2');
-// let display = document.getElementById('output')
-
-// function Max(){
-//     answer = Math.max(...array)
-//     display.value = answer.tostring()
-// }
-// function Min(){
-//     answer = Math.min(...array) 
-//     display.value = answer.tostring()
-// }
-
-// Btn1.addEventListener('click',Max)
-// Btn2.addEventListener('click',Min)
-
 let array = [0, 10, 4, 7, 24, 38, 14, 43, 56];
 let Btn1 = document.getElementById('btn1');
 let Btn2 = document.getElementById('btn2');
@@ -22,12 +5,12 @@ let display = document.getElementById('output');
 
 function Max() {
     let answer = Math.max(...array);
-    display.innerText = answer.toString();
+    display.value = answer.toString();
 }
 
 function Min() {
     let answer = Math.min(...array);
-    display.innerText = answer.toString();
+    display.value = answer.toString();
 }
 
 Btn1.addEventListener('click', Max);
