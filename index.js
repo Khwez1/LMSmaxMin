db3ebfbe6aa3308361c0594029114a736cee34f0
Khwez1 <khwezimbobo@gmail.com>
2023-11-27 11:49:28 +0200

    made some changes

--- index.js
+++ index.js
@@ -1,16 +1,20 @@
-let array = [0, 10, 4, 7, 24, 38, 14, 43, 56];
+// declaring variables
 let Btn1 = document.getElementById('btn1');
 let Btn2 = document.getElementById('btn2');
 let display = document.getElementById('output');
-
+//function for finding and displaying the higher input
 function Max() {
-    let answer = Math.max(...array);
-    display.value = answer.toString();
+    let fstNum = parseInt(document.getElementById('input1').value);
+    let secNum = parseInt(document.getElementById('input2').value);
+    let answer1 = Math.max(fstNum, secNum);
+    display.value = `Max: ` + answer1;
 }
-
+// function for finding and displaying the lower input
 function Min() {
-    let answer = Math.min(...array);
-    display.value = answer.toString();
+    let fstNum = parseInt(document.getElementById('input1').value);
+    let secNum = parseInt(document.getElementById('input2').value);
+    let answer2 = Math.min(fstNum, secNum);
+    display.value = `Min: ` + answer2;
 }
 
 Btn1.addEventListener('click', Max);
